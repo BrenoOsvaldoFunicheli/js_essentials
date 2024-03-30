3adc6abc9486468ba0b45e78bd266164b710e813
BrenoOsvaldoFunicheli <osvaldobreno99@gmail.com>
2024-03-30 13:52:38 -0300

    start JS

---
+++ essentials/vars.js
@@ -0,0 +1,24 @@
+var a = 3
+let x = 40
+
+var a = 3
+// let x = 50 this generate problem (you can't redefine let two times)
+
+
+console.log(a,x)
+
+const m = 2 // don't change
+
+console.log(m)
+
+
+
+let qualquer = 'Legal'
+
+console.log(qualquer)
+
+
+
+qualquer = 'Legals'
+
+console.log(qualquer)
