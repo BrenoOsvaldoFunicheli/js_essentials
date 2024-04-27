ee40fbb2d4cfa45cf82cea4e98b10779b392c8a3
BrenoOsvaldoFunicheli <osvaldobreno99@gmail.com>
2024-04-26 23:43:47 -0300

    adding comment

--- eloquent/09_modules/moduleB.js
+++ eloquent/09_modules/moduleB.js
@@ -3,6 +3,7 @@
 // default exports will be read as variable and other will be get by desctructuring like the following code:
 
 // import defaultvar, {dayName2, dayNumber2}} from "./thisfile.js"
+//defaultvar is a name I gave to the otherfunc that was defined as default export
 
 const names = ["Sunday", "Monday", "Tuesday", "Wednesday",
     "Thursday", "Friday", "Saturday"];
